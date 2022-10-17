f73f8b26db10df469c682db330b3c27f412ddf4a
Griive <igrishkin77@icloud.com>
2022-10-17 11:49:36 +0300

    increased security

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,13 @@
+"""way to add and edit entries;
+create a new form from scratch or use "ModelForm" to save form content to model
+"""
 from django import forms
+# import django forms
 
 from .models import Post
+# import model Post
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ('title', 'text',)
